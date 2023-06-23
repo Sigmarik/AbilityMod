3c9b8193e9f45b2082a33bbe5b03032e5ab68882
Sigmarik <itkudryashov@gmail.com>
2023-06-23 18:19:21 +0300

    initial

---
+++ src/main/java/net/sigmarik/abilitymod/AbilityMod.java
@@ -0,0 +1,12 @@
+package net.sigmarik.abilitymod;
+
+import net.fabricmc.api.DedicatedServerModInitializer;
+
+public class AbilityMod implements DedicatedServerModInitializer {
+	public static final String MOD_ID = "abilitymod";
+
+	@Override
+	public void onInitializeServer() {
+
+	}
+}
