4395dee1e5a6d7a988ef3a23b4dd3bdc32cc1513
Sigmarik <itkudryashov@gmail.com>
2023-06-24 20:30:10 +0300

    feat: persistent trait storage, functional command to add and remove traits from any player, as well as see the list of active traits

--- src/main/java/net/sigmarik/abilitymod/command/TraitCommand.java
+++ src/main/java/net/sigmarik/abilitymod/command/TraitCommand.java
@@ -4,12 +4,15 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.command.suggestion.SuggestionProviders;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.text.Text;
 import net.sigmarik.abilitymod.AbilityMod;
+import net.sigmarik.abilitymod.util.TraitStates;
+
+import java.util.Set;
 
 import static net.minecraft.server.command.CommandManager.*;
 
@@ -24,8 +27,10 @@ public class TraitCommand {
                 .then(literal("remove")
                         .then(argument("players", EntityArgumentType.players())
                         .then(argument("trait", StringArgumentType.word())
-                                .executes(TraitCommand::removeTrait)))
-                )
+                                .executes(TraitCommand::removeTrait))))
+                .then(literal("list")
+                        .then(argument("player", EntityArgumentType.player())
+                                .executes(TraitCommand::printTraits)))
         );
     }
 
@@ -33,9 +38,17 @@ public class TraitCommand {
         String trait = StringArgumentType.getString(context, "trait");
         java.util.Collection<net.minecraft.server.network.ServerPlayerEntity> players =
                 EntityArgumentType.getPlayers(context, "players");
+
+        TraitStates states = TraitStates.getTraitStates(context.getSource().getServer());
+
         for (ServerPlayerEntity player : players) {
             AbilityMod.LOGGER.info("Adding trait " + trait + " to player " + player.getName().getString());
+
+            states.addTrait(player.getUuid(), trait);
         }
+
+        states.markDirty();
+
         return 1;
     }
 
@@ -43,9 +56,35 @@ public class TraitCommand {
         String trait = StringArgumentType.getString(context, "trait");
         java.util.Collection<net.minecraft.server.network.ServerPlayerEntity> players =
                 EntityArgumentType.getPlayers(context, "players");
+
+        TraitStates states = TraitStates.getTraitStates(context.getSource().getServer());
+
         for (ServerPlayerEntity player : players) {
             AbilityMod.LOGGER.info("Removing trait " + trait + " from player " + player.getName().getString());
+
+            states.removeTrait(player.getUuid(), trait);
         }
+
+        states.markDirty();
+
+        return 1;
+    }
+
+    private static int printTraits(CommandContext<ServerCommandSource> context) throws CommandSyntaxException {
+        ServerPlayerEntity player = EntityArgumentType.getPlayer(context, "player");
+
+        TraitStates states = TraitStates.getTraitStates(player.server);
+        Set<String> traits = states.getTraitList(player.getUuid()).getKeys();
+
+        if (traits.isEmpty()) {
+            player.sendMessage(Text.literal(player.getName().getString() + " has no traits."));
+        } else {
+            player.sendMessage(Text.literal(player.getName().getString() + "'s traits:"));
+            for (String trait : traits) {
+                player.sendMessage(Text.literal(trait));
+            }
+        }
+
         return 1;
     }
 }
